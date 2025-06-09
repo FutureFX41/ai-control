8db53b8124b77c944b0de379f140375bb50898ad
FutureFX41 <futurefx41@gmail.com>
2025-06-09 13:38:28 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import FutureFX41Chat from './components/FutureFX41Chat';
 
-
 function App() {
   return (
-    <FutureFX41Chat />
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Panel de Control de IA</h1>
-      <p>Bienvenido, Alvaro 👋</p>
-
-      <div style={{ marginTop: "1rem" }}>
-        <button onClick={() => alert("Ejecutando instrucción AI...")}>
-          Ejecutar tarea automática
-        </button>
-      </div>
+    <div>
+      {/* Tu panel aquí */}
+      <FutureFX41Chat />
     </div>
   );
 }
 
-export default App;
